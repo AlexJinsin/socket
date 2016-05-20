95ac0d052e382e213dde168153b29a36f89afe54
Alex <569486554@qq.com>
2016-05-20 15:51:18 +0800

    双通信的socket
    client必须给server发送一个结束标识，server才能给client返回 要不然会阻塞在 inputstream的读取

--- src/com/alex/socket/base/SocketClient.java
+++ src/com/alex/socket/base/SocketClient.java
@@ -1,7 +1,9 @@
 package com.alex.socket.base;
 
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.io.Reader;
 import java.io.Writer;
 import java.net.Socket;
 
@@ -13,17 +15,41 @@ public class SocketClient {
 
 	public static void main(String[] args) {
 		
+		System.out.println("client");
 		try {
-			Socket client = new Socket("127.0.0.1", 8888);
+			
+//			创建socket client连接  连接本地ip   端口 8888(端口尽量选大一些因为  小的端口系统保留 建议5位数)
+			int port = 8888;
+			Socket client = new Socket("127.0.0.1", port);
+//			向socket server 写入本次需要发送的内容
 			Writer writer = new OutputStreamWriter(client.getOutputStream());
+
 			writer.write("hello world from client");
+			writer.write("eof");//发送 结束才能从server中读取内容
 			writer.flush();
+			
+			
+			
+			Reader reader = new InputStreamReader(client.getInputStream());
+			int len;
+			char buffer[] = new char[10];
+			StringBuffer stringBuffer = new StringBuffer();
+			while ((len = reader.read(buffer)) != -1) {
+				System.out.println(2);
+				stringBuffer.append(new String(buffer,0,len));
+			}
+			
+			System.out.println("client  接到 server的 返回:" + stringBuffer);
+			
+			reader.close();
+
+			
+			
+//			关闭资源 （高版本编译器会提示资源泄漏 ）
 			writer.close();
 			client.close();
-			
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
 	}
 }
--- src/com/alex/socket/base/SocketServer.java
+++ src/com/alex/socket/base/SocketServer.java
@@ -2,7 +2,9 @@ package com.alex.socket.base;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.io.Reader;
+import java.io.Writer;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -13,22 +15,40 @@ public class SocketServer {
 	}
 
 	public static void main(String[] args) {
+		
+		System.out.println("server");
 		try {
-			ServerSocket serverSocket = new ServerSocket(8888);
+//			创建一个socketserver 监听服务端口 8888
+			int listenPort = 8888;
+			ServerSocket serverSocket = new ServerSocket(listenPort);
+//			接受socket连接 阻塞
 			Socket socket = serverSocket.accept();
+//			接受client的输入
 			Reader reader = new InputStreamReader(socket.getInputStream());
-
-			char buffer[] = new char[10];
+			Writer writer = new OutputStreamWriter(socket.getOutputStream());
+//			读取socket client发送的信息
 			int len;
+			char buffer[] = new char[10];
+			String temp;
+			int index;  
 			StringBuffer stringBuffer = new StringBuffer();
+//			流在读取的过程中采用阻塞方式有内容读取没内容 就在这死等
 			while ((len = reader.read(buffer)) != -1) {
-				
+				System.out.println("1");
+				 temp = new String(buffer, 0, len);  
+//				 如果这里不加eof标识   socket会在这里一直读等到文件都读完 
+		         if ((index = temp.indexOf("eof")) != -1) {//遇到eof时就结束接收  
+		        	 stringBuffer.append(temp.substring(0, index));  
+		            break;  
+		         }  
 				stringBuffer.append(new String(buffer,0,len));
-				
 			}
-			
 			System.out.println("收到  来自client的信息" + stringBuffer);
-			
+//			写入返回给客户端的内容 
+			writer.write("server 返回");
+			writer.flush();
+//			关闭资源 防止资源泄漏
+			writer.close();
 			reader.close();
 			socket.close();
 			serverSocket.close();
